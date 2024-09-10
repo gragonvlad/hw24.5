0474d4a2b9039bb7ba82369595046beffb72a5b1
gragonvlad <gragonvlad@gmail.com>
2024-09-10 15:22:33 +0300

    --breeze auth

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,61 +1,23 @@
 import './bootstrap';
+import '../css/app.css';
 
-import $ from 'jQuery'
-import App from './App.vue'
-import { createApp } from 'vue/dist/vue.esm-bundler'
-let app = createApp(App);
-import router from './router';
-import PrimeVue from 'primevue/config';
-import RadioButton from 'primevue/radiobutton';
-import Slider from 'primevue/slider';
-import Button from "primevue/button";
-import Aura from '@primevue/themes/nora';
-app.use(PrimeVue, {
-  pt: {
-    button: {
-        header: 'bg-gray-50 text-gray-900 p-4 border',
-        content: 'border p-4 text-gray-900',
-        footer: 'p-4 border'
-    }
-},
-  theme: {
-      preset: Aura,
-      options: {
-          prefix: 'p',
-          darkModeSelector: 'system',
-          cssLayer: false
-      }
-  },
-  zIndex: {
-    modal: 1100,        //dialog, drawer
-    overlay: 1000,      //select, popover
-    menu: 1000,         //overlay menus
-    tooltip: 1100       //tooltip
-}
+import { createApp, h } from 'vue';
+import { createInertiaApp } from '@inertiajs/vue3';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    setup({ el, App, props, plugin }) {
+        return createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(ZiggyVue)
+            .mount(el);
+    },
+    progress: {
+        color: '#4B5563',
+    },
 });
-app.component('Slider', Slider);
-app.component('Button', Button);
-app.component('RadioButton',RadioButton);
-
-import HomeView from './Test.vue'
-import AboutView from './Test2.vue'
-const Home = { template: '<h2>Home Page</h2>' }
-const About = { template: '<h2>About Page</h2>' }
-
-const routes = [
-  { path: '/', component: Home },
-  { path: '/about', component: About }
-]
-app.use(router)
-//createApp(App).mount("#app")
-
-
-app.mount('#app');
-
-
-$(document).ready(function () {
-console.log('Start jquery');
-    
-   
-});
- console.log('123'); // hello 124
